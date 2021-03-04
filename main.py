6e26176d484800e4c6d543b54c4c976fe15a85ed
Guillaume <guillaume.gautier@altran.com>
2021-03-04 17:23:43 +0100

    Added functional window

--- main.py
+++ main.py
@@ -1,31 +1,59 @@
-import tkinter as tk
+import sys
+from configparser import ConfigParser
+
+from PySide2.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QTabWidget, \
+    QLineEdit
+
+from manager import NPCGenerator
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.setWindowTitle("NPC Generator")
+        self.setFixedWidth(1000)
+        self.npc_config = ConfigParser()
+        self.npc_config.read("npc.ini", "utf8")
+        self.npc = NPCGenerator(self.npc_config)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.central_layout = QVBoxLayout()
+        self.central_widget.setLayout(self.central_layout)
+        self.tabs = QTabWidget()
+        self.central_layout.addWidget(self.tabs)
+        self.nb_npc = 1
+        self.tabs.addTab(GeneratorPanel(self.npc), "NPC 1")
+        self.add_npc_button = QPushButton("Add NPC")
+        self.add_npc_button.clicked.connect(lambda: self.add_npc())
+        self.central_layout.addWidget(self.add_npc_button)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    def add_npc(self):
+        self.nb_npc += 1
+        self.tabs.addTab(GeneratorPanel(self.npc), f"NPC {self.nb_npc}")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+class GeneratorPanel(QWidget):
+    def __init__(self, npc_generator: NPCGenerator, parent=None):
+        super(GeneratorPanel, self).__init__(parent)
+        self.npc = npc_generator
+        self.layout = QVBoxLayout()
+        self.npc_label = QLineEdit()
+        self.layout.addWidget(self.npc_label)
+        self.generate_button = QPushButton("Generate NPC")
+        self.generate_button.clicked.connect(lambda: self.get_generated(self.npc_label))
+        self.layout.addWidget(self.generate_button)
+        self.setLayout(self.layout)
+
+    def get_generated(self, box: QLineEdit):
+        output = self.npc.generate()
+        box.setText(output)
 
 
 def main():
-    root = tk.Tk()
-    app = Application(master=root)
-    app.mainloop()
+    app = QApplication()
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
